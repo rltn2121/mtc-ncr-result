5c9fd9b0b672a1a761bceb70165fdc38110b82b7
gayoung <orz4120@gmail.com>
2023-11-13 21:21:38 +0900

    result

--- src/main/java/result/apis/dto/MtcInsertRequest.java
+++ src/main/java/result/apis/dto/MtcInsertRequest.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class MtcResultRequest {
+public class MtcInsertRequest {
     private String acno;
     private String trxdt;
     private String curC;
--- src/main/java/result/queue/ResultConsumer.java
+++ src/main/java/result/queue/ResultConsumer.java
@@ -1,4 +1,46 @@
 package result.queue;
 
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import result.apis.dto.MtcInsertRequest;
+import result.apis.dto.MtcResultRequest;
+
+@Component
+@RequiredArgsConstructor
 public class ResultConsumer {
+    private static final Logger log = LoggerFactory.getLogger(ResultConsumer.class);
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final WebClient webClient;
+    @KafkaListener(topics = "mtc.ncr.result")
+    public void consumeMessage(@Payload MtcResultRequest resReqInfo ,
+                               @Header(name = KafkaHeaders.RECEIVED_KEY , required = false) String key ,
+                               @Header(KafkaHeaders.RECEIVED_TOPIC ) String topic ,
+                               @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long timestamp ,
+                               @Header(KafkaHeaders.OFFSET) long offset
+    ) {
+        log.info("$$$$$$$$$$$result 큐 읽는다 : {}" , resReqInfo.toString());
+        /*
+        * 거래가 성공이면 바로 거래 큐에 쌓는다
+        * 거래가 실패면 보상 트랜잭션 큐에 넣고 거래내역 큐를 쌓는다.
+        * */
+        MtcInsertRequest insertRequest = new MtcInsertRequest();
+        insertRequest.setAcno(resReqInfo.getAcno());
+        insertRequest.setTrxdt(resReqInfo.getTrxdt());
+        insertRequest.setCurC(resReqInfo.getCurC());
+        insertRequest.setAprvSno(resReqInfo.getAprvSno());
+        insertRequest.setNujkJan(resReqInfo.getNujkJan());
+        insertRequest.setErrMsg(resReqInfo.getErrMsg());
+        insertRequest.setTrxAmt(resReqInfo.getTrxAmt());
+        insertRequest.setUpmuG(resReqInfo.getUpmuG());
+
+        kafkaTemplate.send("mtc.dbs.insertChlGojeong", "" , resReqInfo);
+    }
 }
